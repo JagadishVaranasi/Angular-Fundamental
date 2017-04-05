ad75585e74f6c5f1754c91e0d3627a7a6ece9cfc
JagadishVaranasi <jagadishkumar.varanasi@cognizant.com>
2017-04-05 15:42:06 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
 app.use(express.static(__dirname + '/app'));
 
-// Start the app by listening on the default
-// Heroku port
+app.get('/', function(req, res){
+  res.redirect('/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
