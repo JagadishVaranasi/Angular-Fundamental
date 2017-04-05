c0d1006f05a09946f00a7a63a7269a55b98c9120
JagadishVaranasi <jagadishkumar.varanasi@cognizant.com>
2017-04-05 15:13:35 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/app'));
-//add this so the browser can GET the bower files
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
