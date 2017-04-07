d16112fb8c7fb0430315ffbd7faa2b4062f57385
JagadishVaranasi <jagadishkumar.varanasi@cognizant.com>
2017-04-07 17:14:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
+// Get dependencies
+// https://scotch.io/tutorials/mean-app-with-angular-2-and-the-angular-cli
+// https://expressjs.com/en/starter/static-files.html
+
+
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Point static path to src
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.set('port', (process.env.PORT || 8080));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('/', function (req, res, next) {
+    res.render('index.html');
+});
+
+//Views
+app.set('views',path.join(__dirname,'src'));
+app.set('view engine','ejs');
+ app.engine('html',require('ejs').renderFile);
+
 // Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+// Heroku port or 3000
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'),function() {    
+    console.log('Node server listening on port '+ app.get('port'));
+});
+
+module.exports = app;
